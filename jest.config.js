1a5995f9a76d1eb194f1ac6223f9cae53cd14290
oualid.qannouf <oualid.qannouf@kaibee.fr>
2022-10-01 11:29:34 +0200

    setting up jest and pnpm

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+
+const { compilerOptions } = require('./tsconfig');
+
+
+module.exports = {
+
+  preset: 'jest-preset-angular',
+
+  roots: ['<rootDir>/src/'],
+
+  testMatch: ['**/+(*.)+(spec).+(ts|js)'],
+
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+
+  collectCoverage: true,
+
+  coverageReporters: ['html'],
+
+  coverageDirectory: 'coverage/my-app'
+};
